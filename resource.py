05b021454cada4fba772ea6aa6a6b90a2ee03860
jujopalacio <juanjosepalaciouribe@gmail.com>
2022-02-18 16:38:05 -0500

    Initial app code

---
+++ resource.py
@@ -0,0 +1,8 @@
+import boto3
+
+s3 = boto3.resource("s3")
+bucket = s3.Bucket("ws-bma-test")
+for obj in bucket.objects.all():
+    print(obj.key, obj.last_modified())
+
+s3_client = boto3.resource("s3").meta.client
